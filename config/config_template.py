df07f62ea285dd2c4a70b953342ef7eb79c2e86d
Hannu Kamarainen <hannu.kamarainen@csc.fi>
2018-08-22 10:28:55 +0300

    Move template config from template/ to config/. Add config.py (real conf file) to .gitignore

--- config/config_template.py
+++ config/config_template.py
@@ -1,33 +1,33 @@
 
-
-
 def load_config_variables():
     config_dict = {
-        
+
+        # metax
         "METAX_HOST": "",
         "METAX_USER": "",
         "METAX_PASS": "",
-        
+
+        # etsin
         "ETSIN_HOST": "",
         "ETSIN_USER": "",
         "ETSIN_PASS": "",
 
-        "IDA_HOST":"",        
-        "IDA_USER":"",
+        # ida
+        "IDA_HOST": "",
+        "IDA_USER": "",
         "IDA_PASS": "",
-
         "HTTPD_USER": "apache",
         "ROOT_DIR": "/var/ida",
-        "PROJ_USER_PASS":"test",
+        "PROJ_USER_PASS": "test",
 
-        "DPRES_HOST":"",
-        "DPRES_USER":"",
-        "DPRES_PASS":"",
+        # pas
+        "DPRES_HOST": "",
+        "DPRES_USER": "",
+        "DPRES_PASS": "",
 
-        "QUVAIN_HOST":"",
-        "QUVAIN_USER":"",
-        "QUVAIN_PASS":"",
-
-        
+        # qvain
+        "QUVAIN_HOST": "",
+        "QUVAIN_USER": "",
+        "QUVAIN_PASS": "",
     }
     return config_dict
