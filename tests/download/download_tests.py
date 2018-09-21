7dd32a37a88b240996fcb67210c6e2f71a1091e0
jarve <pj@csc.fi>
2018-09-21 11:56:31 +0300

    CSCFAIRDATAAAI-63 lisätty tiedosto ja hakemsito testit

--- tests/download/download_tests.py
+++ tests/download/download_tests.py
@@ -14,9 +14,22 @@ class TestDownload(unittest.TestCase):
         self.OK = [200, 201, 202, 203, 204]
         self.FAIL = [401, 403, 404, 500]
 
+    def test_file(self):
+        # downloading a file from the example dataset
+        urn = 'urn:nbn:fi:att:d2cf4977-36fa-4762-adb3-126ea06108ed?file=5b7d486c951df671401089f134223'
+        download_status, download_data = download.download_dataset(urn)
+        self.assertIn(download_status, self.OK, "Download could not download the file")
+
+    def test_dir(self):
+        # downloading a dir from the example dataset
+        urn = 'urn:nbn:fi:att:d2cf4977-36fa-4762-adb3-126ea06108ed?dir=c66c97e387933b82a734d904cc9572a3'
+        download_status, download_data = download.download_dataset(urn)
+        self.assertIn(download_status, self.OK, "Download could not download the file")
+
     def test_dataset(self):
         # downloading the example dataset
 
         urn = 'urn:nbn:fi:att:d2cf4977-36fa-4762-adb3-126ea06108ed'
         download_status, download_data = download.download_dataset(urn)
         self.assertIn(download_status, self.OK, "Download could not download the dataset")
+
